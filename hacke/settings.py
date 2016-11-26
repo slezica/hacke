a5a367e59ca88ed1cd540aad5783f19cc887f827
Nicolás Santángelo <santangelonicolas@gmail.com>
2016-11-26 15:05:03 -0300

    Postgres?

--- hacke/settings.py
+++ hacke/settings.py
@@ -12,6 +12,23 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+in_heroku = False
+
+if 'DATABASE_URL' in os.environ:
+    in_heroku = True
+
+import dj_database_url
+
+if in_heroku:
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
