58f53ae54d8b66cd7026c8d182e6006b47b9d267
Nicolás Santángelo <santangelonicolas@gmail.com>
2016-11-26 15:05:14 -0300

    Again

--- hacke/settings.py
+++ hacke/settings.py
@@ -21,13 +21,6 @@ import dj_database_url
 
 if in_heroku:
     DATABASES = {'default': dj_database_url.config()}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
